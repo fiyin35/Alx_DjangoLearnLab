db583d3b125c391f81f577f874ca9703bfec8365
Fiyinfolu <lolafiyin@gmail.com>
2024-10-28 22:57:01 +0100

    created django project called libraryproject

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5jpca&q-cxp3*8v87*ir5=tbpw9gipufzhbp#r)otax5t4&n_o'
+SECRET_KEY = 'django-insecure-)62+ldwrv5q%fuvktb%^xul-b!!ea7&3h#)=_#n1%=m*9bx#nt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
