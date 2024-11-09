dec5ec1dc78f426129c02b9f38279bda5b496f93
Fiyinfolu <lolafiyin@gmail.com>
2024-11-09 19:50:58 +0100

    added return self.name

---
+++ django-models/LibraryProject/bookshelf/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField(null=False)
+
+
+    def __str__(self):
+        return f"{self.title} by {self.author} published in {self.publication_year}"
