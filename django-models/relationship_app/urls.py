b4c7fe6b9bcd00ebcbf3071e0a692d95fc4cffb2
Fiyinfolu <lolafiyin@gmail.com>
2024-11-10 15:38:57 +0100

    fixed the url pattern

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.url import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import book_list, LibraryDetailView, Registation, Login
+from .views import book_list, LibraryDetailView, Registation
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('list-book/', book_list),
     path('library-detail/', LibraryDetailView),
     path('register/', 'views.register', Registation),
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout')
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name="logout")
 ]
