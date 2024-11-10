9c8647381e5267f7d29898fef0ee501814380daf
Fiyinfolu <lolafiyin@gmail.com>
2024-11-10 15:20:01 +0100

    added register, login, logout functionality

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView, CreateView
 from .models import Library
 from .models import Book
-from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
@@ -25,7 +24,7 @@ class LibraryDetailView(DetailView):
           return context
 
 class Registation(CreateView):
-     form_class = UserCreationForm
+     form_class = UserCreationForm()
      success_url = reverse_lazy('login')
      template_name = 'relationship_app/register.html'
 
