ae63ec1ffc2fdbedd40bd28c67f7928bf8e98823
Fiyinfolu <lolafiyin@gmail.com>
2024-11-10 13:34:33 +0100

    added views and url

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,12 +8,12 @@ def book_list(request):
     """lists all books stored in the database"""
     books = Book.objects.all()
     context = {'list_book': books}
-    return render(request, 'books/list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 class BookDetail(DetailView):
      """A class-based view for displaying details for a specific library"""
      model = Library
-     template_name = 'relationship_app/list_books.html'
+     template_name = 'relationship_app/library_detail.html'
      
      def get_context(self, **kwargs):
           context = super().get_context_data(**kwargs)
