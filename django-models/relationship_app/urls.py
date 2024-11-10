6f54527cd4b60d4fd1dec52f328555e7512a0ec7
Fiyinfolu <lolafiyin@gmail.com>
2024-11-10 15:11:32 +0100

    added register, login, logout functionality

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import book_list, LibraryDetailView, Registation, Login
 urlpatterns = [
     path('list-book/', book_list),
     path('library-detail/', LibraryDetailView),
-    path('register/', Registation),
+    path('register/', 'views.register', Registation),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout')
 ]
