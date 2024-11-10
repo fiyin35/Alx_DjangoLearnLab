1eee53d11bc4f11e4571ea3db8836300d110cbf5
Fiyinfolu <lolafiyin@gmail.com>
2024-11-10 22:03:23 +0100

    added permissions to edit, create and delete book

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('admin/', admin_view.as_view(), name='admin'),
     path('library/', library_view.as_view(), name='library'),
     path('member/', member_view.as_view(), name='member'),
-    path('add_book', add_book_view.as_view(), name='add_book'),
-    path('edit_book', edit_book_view.as_view(), name='edit_book'),
-    path('delete_book', delete_book_view.as_view(), name='delete_book'),
+    path('add_book/', add_book_view.as_view(), name='add_book'),
+    path('edit_book/', edit_book_view.as_view(), name='edit_book'),
+    path('delete_book/', delete_book_view.as_view(), name='delete_book'),
 ]
