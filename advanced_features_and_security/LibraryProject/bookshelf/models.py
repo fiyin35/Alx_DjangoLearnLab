f02eb4424f7cdaa5dfb000085889ce983add3176
Fiyinfolu <lolafiyin@gmail.com>
2024-11-17 00:44:58 +0100

    added the customeuser to admin

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -13,10 +13,33 @@ class Book(models.Model):
         return f"{self.title} by {self.author} published in {self.publication_year}"
 
 class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, null=False, blank=False)
+    password = models.CharField(max_length=50, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos', null=True, blank=True)
 
 class CustomUserManager(BaseUserManager):
-    create_user = models.ForeignKey(CustomUser)
-    create_superuser = models.CharField(maxLength=255)
+    def create_user(self, email, password, phone_number, date_of_birth=None, profile_photo=None):
+        if not email:
+            raise ValueError("Please provide an email address")
+        if not password:
+            raise ValueError("Please provide a password")
+        email = self.normalize_email(email)
+        user = self.model(email=email, phone_number=phone_number, date_of_birth=date_of_birth, profile_photo=profile_photo)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password, phone_number, date_of_birth=None, profile_photo=None):
+        user = self.model(email, password, phone_number, date_of_birth, profile_photo)
+        user.setDefault('is_staff', True)
+        user.setDefault('is_superuser', True)
+
+        if user.get('is_staff') is not True and user.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_staff and is_supervisor equal to true") 
+        
+        return self.create_user(email, password, phone_number, date_of_birth, profile_photo)
+
+    
 
