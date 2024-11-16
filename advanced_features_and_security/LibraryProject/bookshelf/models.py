c72c6a5107d17c478b4d32765bd37b2479969e11
Fiyinfolu <lolafiyin@gmail.com>
2024-11-16 23:06:41 +0100

    added custome user

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
@@ -12,6 +12,6 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title} by {self.author} published in {self.publication_year}"
 
-class customeUser(AbstractBaseUser):
+class CustomUser(AbstractUser):
     date_of_birth = models.DateField()
     profile_photo = models.PhotoField()
