5f3675a322c46339ad638313cd39aaf29956ccea
Fiyinfolu <lolafiyin@gmail.com>
2024-11-17 21:53:17 +0100

    security features added in the settings

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm
+from .forms import ExampleForm
 
 # Create your views here.
 def book_list(request):
@@ -11,39 +11,39 @@ def book_list(request):
     return render(request, 'relationship_app/list_books.html', context)
 
 @permission_required('bookshelf.can_create', raise_exception=True)
-def createBook(request):
+def create_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('list-book')
     else:
-        form = BookForm()
+        form = ExampleForm()
     return render(request, 'bookshelf/form_example.html', {'book': form})
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
-def editBook(request, book_id):
+def edit_book(request, book_id):
     """get the book instance to edit using book id"""
     book = Book.get_object_or_404(Book, id=book_id)
 
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=request.book)
+        form = ExampleForm(request.POST, instance=request.book)
         if form.is_valid():
             form.save()
             redirect('list-book')
     else:
         """initialize the form with the current book instance for GET request"""
-        form = BookForm(instance=book)
+        form = ExampleForm(instance=book)
     return render(request, 'bookshelf/edit_book.html', {'book': book})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def viewBook(request, pk):
+def view_book(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         pass
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
-def deleteBook(request, book_id):
+def delete_book(request, book_id):
     book = Book.get_object_or_404(Book, id=book_id)
 
     if request.method == 'POST':
