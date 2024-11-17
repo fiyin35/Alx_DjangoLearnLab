8f4951b502f9e6bf94fe510fd18ad711a6a17036
Fiyinfolu <lolafiyin@gmail.com>
2024-11-17 21:44:32 +0100

    security features added in the settings

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -23,7 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)62+ldwrv5q%fuvktb%^xul-b!!ea7&3h#)=_#n1%=m*9bx#nt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+SECURE_BROWSER_XSS_FILTER = False
+
+X_FRAME_OPTIONS = 'DENY'
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = []
 
