3a2ff247dd38de9f7b2ac0203b90a6114d407de4
Fiyinfolu <lolafiyin@gmail.com>
2024-11-24 17:52:30 +0100

    list, create, update and delete apis

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,13 +1,13 @@
 from .models import Book 
 from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView
-from rest_framework.viewsets import ModelViewSet
+import rest_framework 
 
 # Create your views here.
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewSet(ModelViewSet):
+class BookViewSet(rest_framework.viewsets.ModelViewSet):
     queryset = Book.objects.all()  # Retrieves all Book instances
     serializer_class = BookSerializer
