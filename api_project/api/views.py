9d416cd90ac4f8fc87ffddf33287d72e243e7682
Fiyinfolu <lolafiyin@gmail.com>
2024-11-24 08:01:51 +0100

    added the serializers, cofigure the urls and the views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,8 @@
 from .models import Book 
 from .serializers import BookSerializer
-from rest_framework.generics import ListAPIView
+import rest_framework
 
 # Create your views here.
-class BookList(ListAPIView):
+class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
