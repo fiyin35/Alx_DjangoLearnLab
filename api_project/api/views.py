39a408be613a928d5aab27c40593581c184b9664
Fiyinfolu <lolafiyin@gmail.com>
2024-11-24 07:53:34 +0100

    added the serializers, cofigure the urls and the views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Book 
+from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
 
 # Create your views here.
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
