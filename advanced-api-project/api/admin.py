c747f3a59f63ae6ba0f53705dc4eebb4b0ab2f1a
Fiyinfolu <lolafiyin@gmail.com>
2024-11-29 15:34:34 +0100

    created custome serializers

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book, Author
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year', 'author')
+
+    search_fields = ('author', 'title')
+
+
+admin.site.register(Book, Author, BookAdmin)
--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,13 +1,27 @@
 from rest_framework import serializers
 from .models import Book, Author
+import datetime
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['title', 'publication_year', 'author']
+        
+    # function to validate the publication year to ensure it's not above the current year
+    # which means publication_year can not be set to future date
+    def publication_year_validation(self, value):
+        current_year = datetime.date.today().year
+
+        if value > current_year:
+            serializers.ValidationError("Publication year can not be in the future")
+        
+        return value
+
 
 class AuthorSerializer(serializers.ModelSerializer):
+
+    books = BookSerializer(many=True, read_only=True, source='book_set')
     class Meta:
         model = Author
         field = ['author']
