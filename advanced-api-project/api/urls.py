8b116166682b534cc2375202ecfca9e27188bef7
Fiyinfolu <lolafiyin@gmail.com>
2024-11-29 17:44:45 +0100

    added the urls patterns

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views
-from .views import ListView, DetailView, CreateView
+from .views import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 urlpattern = [
     path('books/', ListView.as_view()),
-    path('books/<int:pk>/', DetailView.as_view()),
-    path('books/add/', CreateView.as_view()),
+    path('books/<int:pk>/', DetailView),
+    path('books/create/', CreateView),
+    path('books/update/', UpdateView),
+    path('books/delete/', DeleteView)
 ]
