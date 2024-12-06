7e5cb04d495d2281fd1e4ddce19350db609d34f5
Fiyinfolu <lolafiyin@gmail.com>
2024-12-07 00:32:07 +0100

    updated the forms with tags

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,12 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.TextField(blank=True, null=True, max_length=35, unique=True)
+
+    def __str__(self):
+        return self.name
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
@@ -16,3 +26,5 @@ class Comment(models.Model):
     content = models.TextField(blank=False, null=False)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+
