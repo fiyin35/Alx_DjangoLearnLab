1c7b96a26f033e8b8afd87c1537834e46b345cce
Fiyinfolu <lolafiyin@gmail.com>
2024-12-06 08:04:42 +0100

    added the comment model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,3 +9,10 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
