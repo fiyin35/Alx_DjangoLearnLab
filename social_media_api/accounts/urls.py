388ffd9267e6842595019d3c8621ce951b519d7e
fiyin <lolafiyin@gmail.com>
2024-12-19 19:42:56 +0100

    adjusted the url to user id from pk

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/<int:pk>/', ProfileView.as_view(), name='profile'),
-    path('follow/<int:pk>/', FollowUserView.as_view(), name='followuser'),
-    path('unfollow/<int:pk>/', UnfollowUserView.as_view(), name='unfollowuser')
+    path('follow/<intuser_id>/', FollowUserView.as_view(), name='followuser'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollowuser')
 ]
