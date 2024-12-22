7b1ee169ef4f2e2e98e2ee3007c5f0f152fa4048
fiyin <lolafiyin@gmail.com>
2024-12-22 23:52:39 +0100

    updated the settings.py for production deployment

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -109,10 +109,21 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dbname',
+        'USER': 'dbuser',
+        'PASSWORD': 'dbpassword',
+        'HOST': 'localhost',  # or the IP address of your database server
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
 
